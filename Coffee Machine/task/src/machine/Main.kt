30630a08ec40e156dbeab03e4f01f069aadb43fe
Eder Martinez <edermtz.04@gmail.com>
2022-11-29 21:31:30 -0600

    Finished Stage 2

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,6 +1,16 @@
 package machine
 
 fun main() {
+    println("Write how many cups of coffee you will need:")
+    val cups: Int = readln().toInt()
+    val water: Int = 200
+    val milk: Int = 50
+    val coffee: Int = 15
+    println("For $cups cups of coffee you will need:")
+    println("${cups * water} ml of water")
+    println("${cups * milk} ml of milk")
+    println("${cups * coffee} gr of coffee beans")
+    /*
     println("Starting to make a coffee")
     println("Grinding coffee beans")
     println("Boiling water")
@@ -8,4 +18,5 @@ fun main() {
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
     println("Coffee is ready!")
+    */
 }
